4362302de08a5b056e858067b2d7c9c44ba93140
Yaron Torbaty <yarontorbaty@gmail.com>
2025-12-10 15:09:17 +0200

    Fix Android.mk: move GStreamer include before our module definition
    The gstreamer-1.0.mk must be included before defining our orbistream_native
    module, otherwise the source files (orbistream_jni.cpp, srt_streamer.cpp)
    were not being compiled at all.
    
    This fixes the missing Java_org_freedesktop_gstreamer_GStreamer_nativeInit
    JNI symbol that was causing GStreamer initialization to fail.

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -17,32 +17,7 @@ else ifeq ($(TARGET_ARCH_ABI),x86_64)
 endif
 
 #######################################
-# OrbiStream native library
-#######################################
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := orbistream_native
-LOCAL_SRC_FILES := \
-    orbistream_jni.cpp \
-    srt_streamer.cpp
-
-LOCAL_SHARED_LIBRARIES := gstreamer_android
-LOCAL_LDLIBS := -llog -landroid
-
-LOCAL_C_INCLUDES := \
-    $(GSTREAMER_ROOT)/include/gstreamer-1.0 \
-    $(GSTREAMER_ROOT)/include/glib-2.0 \
-    $(GSTREAMER_ROOT)/lib/glib-2.0/include \
-    $(GSTREAMER_ROOT)/include
-
-LOCAL_CPPFLAGS := -std=c++17 -DGSTREAMER_AVAILABLE=1
-LOCAL_CFLAGS := -DGSTREAMER_AVAILABLE=1
-
-include $(BUILD_SHARED_LIBRARY)
-
-#######################################
-# GStreamer integration
+# GStreamer integration (must be first)
 #######################################
 
 ifndef GSTREAMER_SDK_ROOT_ANDROID
@@ -80,3 +55,27 @@ GSTREAMER_EXTRA_DEPS := gstreamer-video-1.0 gstreamer-audio-1.0 gstreamer-app-1.
 # Include GStreamer build integration
 include $(GSTREAMER_ROOT)/share/gst-android/ndk-build/gstreamer-1.0.mk
 
+#######################################
+# OrbiStream native library
+#######################################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := orbistream_native
+LOCAL_SRC_FILES := \
+    orbistream_jni.cpp \
+    srt_streamer.cpp
+
+LOCAL_SHARED_LIBRARIES := gstreamer_android
+LOCAL_LDLIBS := -llog -landroid
+
+LOCAL_C_INCLUDES := \
+    $(GSTREAMER_ROOT)/include/gstreamer-1.0 \
+    $(GSTREAMER_ROOT)/include/glib-2.0 \
+    $(GSTREAMER_ROOT)/lib/glib-2.0/include \
+    $(GSTREAMER_ROOT)/include
+
+LOCAL_CPPFLAGS := -std=c++17 -fexceptions -frtti -DGSTREAMER_AVAILABLE=1
+LOCAL_CFLAGS := -DGSTREAMER_AVAILABLE=1
+
+include $(BUILD_SHARED_LIBRARY)
